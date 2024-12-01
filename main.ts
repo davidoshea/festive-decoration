b81be06b8c2c2a277f2692b8a8ad4afa0c373aa6
davidoshea <44163829+davidoshea@users.noreply.github.com>
2024-12-01 21:08:05 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showNumber(input.lightLevel())
+})
+function doLights () {
+	
+}
 basic.forever(function () {
+    basic.pause(500)
     if (input.lightLevel() < 255) {
         basic.showLeds(`
             . . . . .
@@ -39,6 +53,8 @@ basic.forever(function () {
             . . . . .
             # . # . #
             `)
+        basic.pause(500)
+        basic.clearScreen()
         basic.pause(500)
     } else {
         basic.clearScreen()
