c2758d90ab784df7b709505377a33ac18caaf01e
davidoshea <44163829+davidoshea@users.noreply.github.com>
2024-12-07 20:37:09 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,105 +5,10 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
     basic.pause(500)
 })
-input.onGesture(Gesture.LogoUp, function () {
-    basic.pause(slowness2)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . # . .
-        . # . # .
-        . . . . .
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        # . . . #
-        . # # # .
-        . # . # .
-        . # # # .
-        # . . . #
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        # . # . #
-        . # . # .
-        # . . . #
-        . # . # .
-        # . # . #
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        # . # . #
-        . # . # .
-        # . . . #
-        . # . # .
-        # . # . #
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        # . . . #
-        . # # # .
-        . # . # .
-        . # # # .
-        # . . . #
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . # . .
-        . # . # .
-        . . . . .
-        `)
-    basic.pause(slowness2)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(slowness2)
-    basic.clearScreen()
-    basic.pause(slowness2)
-})
-input.onGesture(Gesture.ScreenUp, function () {
+input.onButtonPressed(Button.AB, function () {
     led.stopAnimation()
     basic.clearScreen()
-    n1 = 0
-    n2 = 1
-    for (let index = 0; index <= 12; index++) {
+    for (let index = 0; index <= 10; index++) {
         n3 = n2
         n3 += n1
         n1 = n2
@@ -122,6 +27,99 @@ input.onButtonPressed(Button.B, function () {
         basic.pause(500)
     }
 })
+input.onGesture(Gesture.ThreeG, function () {
+    basic.pause(slowness2)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . # . # .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . . . #
+        . # . # .
+        # . # . #
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . . . #
+        . # . # .
+        # . # . #
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . # . # .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    basic.pause(slowness2)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(slowness2)
+    basic.clearScreen()
+    basic.pause(slowness2)
+})
 let n3 = 0
 let n2 = 0
 let n1 = 0
@@ -129,3 +127,5 @@ let slowness: number
 slowness = 1
 let slowness2: number
 slowness2 = 1
+n1 = 0
+n2 = 1
